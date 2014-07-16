449e8a04e91aa9083b0e76b95be21d46881137ff
Julian Currie <julian.currie@gmail.com>
2014-07-15 17:54:04 -0700

    Added retrofit API layer for offers

---
+++ app/src/main/java/com/julian/bespokeoffers/OffersAPI.java
@@ -0,0 +1,14 @@
+package com.julian.bespokeoffers;
+
+import retrofit.Callback;
+import retrofit.client.Response;
+import retrofit.http.GET;
+
+/**
+ * Created by juliancurrie on 7/15/14.
+ */
+public interface OffersAPI {
+
+    @GET("/mobile-api/v1/offers.json?page_size=10&page=1")
+    void offerList(Callback<Response> callback);
+}
